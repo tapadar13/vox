0473627d6675083bfb026172aa729dd600374a61
tapadar13 <tapadar@prodgain.com>
2026-08-25 15:07:13 +0530

    feat: bootstrap core Tauri plugins

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,22 @@
+use tauri_plugin_autostart::MacosLauncher;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = tauri::Manager::get_webview_window(app, "dashboard") {
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }))
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(
+            tauri_plugin_autostart::Builder::new()
+                .macos_launcher(MacosLauncher::LaunchAgent)
+                .build(),
+        )
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .run(tauri::generate_context!())
+        .expect("error while running Vox");
+}
