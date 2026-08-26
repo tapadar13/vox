989f1dc8947b5535a7a858c5f7ef3b2bf762dfff
tapadar13 <tapadar@prodgain.com>
2026-08-26 09:39:59 +0530

    feat: allow Vox to restart after updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -41,6 +41,7 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_nspanel::init())
         .invoke_handler(tauri::generate_handler![
